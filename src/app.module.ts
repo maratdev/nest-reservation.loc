07f4a1acbce5bc49417def70d38becae2b177080
maratdev <starspersona@gmail.com>
2024-01-19 18:34:42 +0300

    feat: Создание модулей расписания и комнат

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TimetableModule } from './timetable/timetable.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [],
+  imports: [TimetableModule, RoomsModule],
   controllers: [AppController],
   providers: [AppService],
 })
